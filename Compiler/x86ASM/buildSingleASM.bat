2de883006523a5046f36d1b7cc3f12e34ba6313f
Brandon Hilde <bjames.12299@gmail.com>
2024-08-09 21:13:01 -0700

    Update buildSingleASM.bat

--- Compiler/x86ASM/buildSingleASM.bat
+++ Compiler/x86ASM/buildSingleASM.bat
@@ -6,5 +6,6 @@ qemu-system-x86_64 -fda %1.bin
 
 :: this command in the terminal from the Compiler\x86ASM folder will run the hello world program
 :: if you have qemu-system-x86_64 and nasm installed
-:: & .\buildASM.bat example sectortwo
+:: & .\buildSingleASM.bat example
+:: this builds an example.asm file into a example.bin and runs it with qemu
 :: Build system will improve over time
