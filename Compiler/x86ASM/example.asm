90428d22946773f3dd4b992bdac69920b8cfa7a1
unknown <bjames.12299@gmail.com>
2024-09-23 16:33:13 -0700

    int to string in asm

--- Compiler/x86ASM/example.asm
+++ Compiler/x86ASM/example.asm
@@ -1,6 +1,3 @@
-
-;ASMx86 CODE
-
 ORG 0x7c00
 
 ;{CONSTANTS}
@@ -12,21 +9,53 @@ start:
    mov ss, ax
    mov sp, 0x7c00
 
-   mov si, VALUE_0
+   mov si, VALUE_hello
+   call print_log
+   mov si, VALUE_newline
    call print_log
 
-   ;mov al, [scan_code_table + bx]
+   mov eax, [VALUE_value]
+   call convert_int_to_str
 
-   ;mov bl, byte [KeyCodeValue]
-   ;add bx, key_down_table
-   ;mov byte [key_down_table + bx], 0x01
-
-   mov dword [VALUE_array + ebx], eax
-   mov eax, [VALUE_array + ebx]
-
+   mov si, VALUE_str_buffer
+   call print_log
 ;{CODE}
 
    jmp $
+
+prep_convert:
+   mov cl, 0
+   mov [VALUE_str_index], cl
+   ret
+convert_int_to_str:
+   cmp eax, 10
+   jge .div_num
+
+   jmp .store_value
+.div_num:
+   xor edx, edx
+   mov ebx, 10
+   div ebx
+   push edx
+   call convert_int_to_str
+   pop edx
+   mov al, dl
+
+.store_value:
+   add al, '0'
+
+   push ebx 
+   mov ebx, VALUE_str_buffer
+   mov cl, [VALUE_str_index]
+   add ebx, ecx
+   mov byte [ebx], al
+   inc cl
+   mov [VALUE_str_index], cl
+   pop ebx
+    
+
+.done:
+   ret
 
 print_int:
     push bp
@@ -72,14 +101,11 @@ print_log:
 
 ;{INCLUDE}
 
-VALUE_array: times 20 dd 0 
-VALUE_newline db '',13,10,'',0
-VALUE_i dd 12
-VALUE_y dd 7
-VALUE_t dd 3
-VALUE_val dd 0
-VALUE_0 db 'test',0
-VALUE_1 db 'hello',0
+VALUE_str_buffer db 0,0,0,0,0,0,0,0,0,0,0
+VALUE_str_index db 0
+VALUE_newline db 13,10,0
+VALUE_hello db 'hello, world',0
+VALUE_value dd 21
 ;{VARIABLE}
 times 510-($-$$) db 0
 dw 0xaa55
